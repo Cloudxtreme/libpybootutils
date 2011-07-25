c1d22a6a3da99d6a6eeb7f29f9add2a0261eda3e
webnull <webnull.www@gmail.com>
2011-07-25 19:02:12 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2
+from distutils.core import setup
+
+setup(name='libpybootutils',
+      version='1.2',
+      package_dir={'': 'src'},      
+      packages=['libpybootutils']
+     )
---
+++ src/libpybootutils/__init__.py
@@ -0,0 +1,11 @@
+""" Simple bootloader configs parsers for Python. Supports GRUB 0.97 and GRUB 2 
+    To manipulate on GRUB 0.97 you have to import libpybootutils.grubLegacy
+    For GRUB 2 there is libpybootutils.grub class.
+
+    Author: webnull<dot>www(at gmail.com)
+    Jabber: webnull<at>ubuntu(.pl)
+"""
+
+import grub
+import grubLegacy
+# i hope there will be also SysLinux and Lilo class soon
